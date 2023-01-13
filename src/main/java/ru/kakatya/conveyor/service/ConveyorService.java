103461e7cc47470ab7f1d0986d4b3e220323ad96
Ekaterina Akulenko <akule011@gmail.com>
2023-01-13 14:12:21 +0400

    Refactoring

--- src/main/java/ru/kakatya/conveyor/service/ConveyorService.java
+++ src/main/java/ru/kakatya/conveyor/service/ConveyorService.java
@@ -161,13 +161,9 @@ public class ConveyorService {
 
     private BigDecimal calculateMonthlyPayment(BigDecimal amount, BigDecimal rate, Integer term) {
         LOGGER.info("Calculate monthly payment");
-        //p=rate/100/12
-        BigDecimal p = rate.setScale(8, RoundingMode.HALF_DOWN)
-                .divide(new BigDecimal("100.0"), RoundingMode.HALF_DOWN).setScale(8, RoundingMode.HALF_UP)
-                .divide(new BigDecimal("12.0"), RoundingMode.HALF_DOWN).setScale(8, RoundingMode.HALF_UP);
 
         // (1+p)^term
-        BigDecimal denominatorMinuend = p.add(BigDecimal.ONE).pow(term)
+        BigDecimal denominatorMinuend = calculateP(rate).add(BigDecimal.ONE).pow(term)
                 .setScale(24, RoundingMode.HALF_UP);
 
         // denominator=denominatorMinuend-1
@@ -175,10 +171,10 @@ public class ConveyorService {
                 .setScale(24, RoundingMode.HALF_UP);
 
         // fraction=p/denominator
-        BigDecimal fraction = p.divide(denominator, RoundingMode.HALF_UP).setScale(24, RoundingMode.HALF_UP);
+        BigDecimal fraction = calculateP(rate).divide(denominator, RoundingMode.HALF_UP).setScale(24, RoundingMode.HALF_UP);
 
         //secondMultiplier=p+fraction
-        BigDecimal secondMultiplier = p.add(fraction)
+        BigDecimal secondMultiplier = calculateP(rate).add(fraction)
                 .setScale(24, RoundingMode.HALF_UP);
         BigDecimal result = amount.setScale(24, RoundingMode.HALF_UP)
                 .multiply(secondMultiplier).setScale(24, RoundingMode.HALF_UP);
